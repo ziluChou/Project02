ba18cf7acf6a31aa278561b9a3ccb87ee31749b9
zjw <zjw353394422@foxmail.com>
2021-06-26 22:06:05 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/java/Test.java
@@ -0,0 +1,4 @@
+package com.atguigu.java;
+
+public class Test {
+}
---
+++ src/com/atguigu/java/HelloWrold.java
@@ -0,0 +1,7 @@
+package com.atguigu.java;
+
+public class HelloWrold {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!!!");
+    }
+}
